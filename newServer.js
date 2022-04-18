c35526054ddce5288df3fbdd0094081475a3d3ad
Isabel Abonitalla <isabelabonitalla@gmail.com>
2022-04-18 10:49:49 -0400

    sql and app engine work individually but not connected

---
+++ newServer.js
@@ -0,0 +1,147 @@
+// soy-tower-235602:us-central1:fullstack-todo
+
+'use strict';
+
+const express = require('express');
+// const mysql = require('promise-mysql');
+const mysql = require("mysql2");
+const fs = require('fs');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+const config = {
+  connectionLimit: 5,
+  connectTimeout: 10000, // 10 seconds
+  acquireTimeout: 10000, // 10 seconds
+  waitForConnections: true, // Default: true
+  queueLimit: 0, // Default: 0
+};
+
+
+// const db = mysql.createPool({
+//   // host: process.env.DB_HOST,
+//   // port: process.env.DB_PORT,
+//   // user: process.env.DB_USER,
+//   // password: process.env.DB_PASS,
+//   // database: process.env.DB_NAME,
+//   // socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+//   host: "127.0.0.1",
+//   // host: "35.194.41.107",
+//   port: "3306",
+//   user: "root",
+//   password: "0998",
+//   database: "todo",
+//   socketPath: "/cloudsql/soy-tower-235602:us-central1:fullstack-todo",
+//   ...config,
+// });
+
+// db.getConnection((err, connection) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log('Connected to database');
+// });
+
+const createTcpPool = async config => {
+  // Extract host and port from socket address
+  const dbSocketAddr = process.env.DB_HOST.split(':');
+
+  // Establish a connection to the database
+  return mysql.createPool({
+    user: "root", // e.g. 'my-db-user'
+    password: "0998", // e.g. 'my-db-password'
+    database: "todo", // e.g. 'my-database'
+    host: "127.0.0.1", // e.g. '127.0.0.1'
+    port: "3306", // e.g. '3306'
+    // ... Specify additional properties here.
+    ...config,
+  });
+};
+
+const createPool = async () => {
+  const config = {
+    connectionLimit: 5,
+    connectTimeout: 10000, // 10 seconds
+    acquireTimeout: 10000, // 10 seconds
+    waitForConnections: true, // Default: true
+    queueLimit: 0, // Default: 0
+  };
+
+  return createTcpPool(config);
+};
+
+const ensureSchema = async pool => {
+  // Wait for tables to be created (if they don't already exist).
+  await pool.query(
+    `CREATE TABLE IF NOT EXISTS todolist
+      ( taskID INT AUTO_INCREMENT, task VARCHAR(255) NOT NULL, status VARCHAR(255), priority INT, due DATE, PRIMARY KEY (taskID) );`,
+  );
+  console.log("Ensured that table 'todolist' exists");
+};
+
+const createPoolAndEnsureSchema = async () =>
+  await createPool()
+    .then(async pool => {
+      await ensureSchema(pool);
+      return pool;
+    })
+    .catch(err => {
+      logger.error(err);
+      throw err;
+    });
+
+let pool;
+
+app.use(async (req, res, next) => {
+  if (pool) {
+    return next();
+  }
+  try {
+    pool = await createPoolAndEnsureSchema();
+    next();
+  } catch (err) {
+    logger.error(err);
+    return next(err);
+  }
+});
+
+app.get('/get', (req, res) => {
+  db.query(
+    `SELECT * FROM tasks`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+      res.send(results);
+    },
+  );
+  return res;
+});
+
+app.post('/post', (req, res) => {
+  db.query(
+    `INSERT INTO todolist (task, status, priority, due) VALUES (?, ?, ?, ?)`,
+    [req.body.task, req.body.status, req.body.priority, req.body.due],
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+      res.send(results);
+    },
+  );
+});
+
+const PORT = process.env.PORT || 8080;
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+process.on('unhandledRejection', err => {
+  console.error(err);
+  throw err;
+});
+
+module.exports = server;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <p>Hello World!</p>
-    </div>
-  );
-}
 
-export default App;
+export default class Form extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      task: 'didn\'t work',
+      status: '',
+      priority: 2,
+      due: new Date(),
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getData = this.getData.bind(this);
+  };
+
+  async getData() {
+    await fetch('/get', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(console.log('fetching'))
+      .then(res => res.text())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          task: data.task,
+          status: data.status,
+          priority: data.priority,
+          due: data.due,
+        });
+      })
+      .then(console.log('fetching done'))
+      .catch(err => console.log(err))
+      .then(console.log(this.state.task))
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('/post', {
+      method: 'POST',
+      body: JSON.stringify(this.state)
+    }).then(function(response) {
+      console.log(response);
+      return response.json();
+    });
+  }
+
+  render() {
+   return (
+    <>
+      {/* {this.state.task} */}
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Task:
+          <input type="text" value={this.state.value} name="task" onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <button onClick={this.getData}>Get Data</button>
+    </>
+   )
+  }
+ }
